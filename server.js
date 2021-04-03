c37ee4b16f5cdc27538a5d71c8d77a8b88c9a864
Osamudiame  Akioya <jonathanakioya@gmail.com>
2021-04-03 13:37:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,34 @@
 const http = require('http');
 
 //Create a server with variable
-http.createServer(function (req, res) {});
+const server = http.createServer(function (req, res) {
+  //Create headers
+  //res.writeHead(200, { 'content-type': 'text/plain' });
+  //res.writeHead(200, { 'content-type': 'application/json' });
+  //res.writeHead(200, { 'content-type': 'text/html' });
+
+  //Send back some information
+  res.end(`
+  {
+      "Name": "Osamudiame"
+     "Country": "Nigeria"
+     "Hobby": "reading, research, learning new things"
+  }
+   `);
+  //res.end(`
+  // <html>
+  //<body style="background: black; text-align: center; color: white;">
+  //<h1>
+  // This is my backend task for Zuri internship
+  //</h1>
+  //  </body>
+
+  //</html>
+
+  // `);
+});
+
+//Create a port
+server.listen(4000, '127.0.0.1');
+
+console.log('Yes, you have created a server');
